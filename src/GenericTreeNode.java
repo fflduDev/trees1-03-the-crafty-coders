3f366cf70a6cdbdd155c448712c53ea33c6472f7
Dan <dbipen@yahoo.com>
2026-03-31 12:08:58 -0400

    generictreenodesrc

--- src/GenericTreeNode.java
+++ src/GenericTreeNode.java
@@ -8,7 +8,7 @@ public class GenericTreeNode<E> {
 	
 	public GenericTreeNode(E theItem) {
 		data = theItem;
-		children = new ArrayList<>();
+		children = new ArrayList<GenericTreeNode<E>>();
 	}
 	
 	public void addChild(GenericTreeNode<E> theItem) {
@@ -19,7 +19,20 @@ public class GenericTreeNode<E> {
 		// this one is a little harder.
 		// what do you do when the item has children?
 		// I suggest "give them to the parent"
+		// bababababab
+		GenericTreeNode<E> toRemove = null;
+		for (GenericTreeNode<E> child : children) {
+			if (child.data.equals(theItem)) {
+				toRemove = child;
+				break;
+			}
+		}
+		if (toRemove != null) {
+			children.addAll(toRemove.children);
+			children.remove(toRemove);
+		}
 	}
 	
 	
 } 
+
